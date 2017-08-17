b9168deddd67d85f503f15bb9c920f6e156d9537
dylan_DPC <dylan.dpc@gmail.com>
2017-08-18 02:19:32 +0530

    factory modified to inject App instead of using helper

--- src/ShopifyFactory.php
+++ src/ShopifyFactory.php
@@ -2,13 +2,26 @@
 
 namespace ThemeAnorak\LaravelShopify;
 
+use Illuminate\Contracts\Foundation\Application;
 use ThemeAnorak\LaravelShopify\Contracts\ShopifyFactoryContract;
 
 class ShopifyFactory implements ShopifyFactoryContract
 {
+    protected $app;
+
+    /**
+     * ShopifyFactory constructor.
+     * @param $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+
     public function __get($method)
     {
         $class = __NAMESPACE__ . '\\Modules\\' . ucfirst($method);
-        return app($class);
+        return $this->app->make($class);
     }
 }
