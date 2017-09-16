e6bcea26e1e18fc5468636e66e1bf1c2a5d63a4d
dylan_DPC <dylan.dpc@gmail.com>
2017-09-17 01:52:24 +0530

    auth check added to MW

--- src/Middleware/ShopifyAuthorisation.php
+++ src/Middleware/ShopifyAuthorisation.php
@@ -3,6 +3,7 @@
 namespace ThemeAnorak\LaravelShopify\Middleware;
 
 use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use ThemeAnorak\LaravelShopify\Contracts\ShopifyFactoryContract;
@@ -11,6 +12,11 @@ class ShopifyAuthorisation
 {
     public function handle(Request $request, Closure $next, ShopifyFactoryContract $factory)
     {
+        $user = Auth::user();
+        if (!$user) {
+            throw new AuthenticationException();
+        }
+
         if (!$request->header('token')) {
             $factory->auth->getAuthorisationUrl(Auth::user());
         }
