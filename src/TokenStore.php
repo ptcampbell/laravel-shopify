dee716fdfadb50e7d724e90a59394039609407e0
dylan_DPC <dylan.dpc@gmail.com>
2017-10-09 01:15:47 +0530

    token store changed to facade

--- src/TokenStore.php
+++ src/TokenStore.php
@@ -4,34 +4,25 @@ namespace ThemeAnorak\LaravelShopify;
 
 use Illuminate\Contracts\Cache\Store;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 use ThemeAnorak\LaravelShopify\Contracts\TokenStoreContract;
 
 class TokenStore implements TokenStoreContract
 {
 
-    protected $cache;
-
-    /**
-     * TokenStore constructor.
-     * @param $cache
-     */
-    public function __construct(Store $cache)
-    {
-        $this->cache = $cache;
-    }
 
     public function get(Model $user): ?string
     {
-        return $this->cache->get($user->id . 'shopify_token');
+        return Cache::get($user->id . 'shopify_token');
     }
 
     public function set(Model $user, string $token): void
     {
-        $this->cache->put($user->id . 'shopify_token', $token, config('shopify.token_timout', 60));
+        Cache::put($user->id . 'shopify_token', $token, config('shopify.token_timout', 60));
     }
 
     public function forget(Model $user): bool
     {
-        return $this->cache->forget('token');
+        Cache::forget('token');
     }
 }
